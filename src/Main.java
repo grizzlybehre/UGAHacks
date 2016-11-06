d12a935fa99b0548b842bbcc1e846813570279de
grizzlybehre <hayden.behre@gmail.com>
2016-11-05 23:03:50 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,123 @@
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		Scanner keyboard = new Scanner(System.in);
+		Random random = new Random();
+		DamageCalcs battle = new DamageCalcs();
+		boolean areCharactersEqual;
+		System.out.println("Hello, and welcome to the Mutliverse!");
+		String s = "";
+		System.out.println("Please enter the number corresponding to your preferred fighter.");
+		s += "1. Mario" + "\n" + "2. Link" + "\n" + "3. Deadpool" + "\n" + "4. Thomas Edison";
+		System.out.println(s);
+		int choice = keyboard.nextInt();
+		Stats character = new Stats(choice);
+		System.out.println("Here are " + character.getName() + "'s stats.");
+		System.out.println(character.displayInfo());
+		
+		System.out.println("Would you like to fight a random enemy? Y/N");
+		String answer = keyboard.next();
+		
+		Stats enemy = new Stats(0);
+		do{
+		if (answer.equalsIgnoreCase("y")){
+			
+			int rand = random.nextInt(5);
+			while (rand == 0 || rand == choice){
+				rand = random.nextInt(5);
+			}
+			enemy = new Stats(rand);
+		} else {
+			System.out.println("Alright. Here are your choices. Enter the number corresponding to your choice.");
+			System.out.println(s);
+			int chose = keyboard.nextInt();
+			enemy = new Stats(chose);
+		}
+		
+			areCharactersEqual = character.getName().equals(enemy.getName());
+			if(areCharactersEqual){
+				System.out.println("You cannot make two of the same character fight each other. Please make another selection.");
+			}
+			
+		}while(areCharactersEqual);
+		
+		System.out.println("Here are " + enemy.getName() + "'s stats.");
+		System.out.println(enemy.displayInfo());
+		
+		System.out.println("BEGIN");
+		while (4 == 4){
+			System.out.println(" ");
+			System.out.println("Choose your move.");
+			System.out.println(character.getMoves());
+			int move = keyboard.nextInt();
+			
+			character.decDefense(character, enemy);
+			Stats.incTurn();
+			
+			if(character.getSpeed() > enemy.getSpeed()){
+				battle.turnDamage(character, enemy);
+				System.out.println(character.getName() + " used " + character.getMove(move));
+				if (enemy.getCurHealth() <= 0){
+					System.out.println("It did " + battle.getMoveDamage(character, enemy) + " points of damage.");
+					System.out.println(character.getName() + " has won!");
+					break;
+				} else {
+					System.out.println("It did " + battle.getMoveDamage(character, enemy) + " points of damage.");
+					System.out.println(enemy.getName() + " has " + enemy.getCurHealth() + " points left");
+				}
+				
+				battle.turnDamage(enemy, character);
+				int rand2 = random.nextInt(5);
+				while (rand2 == 0){
+					rand2 = random.nextInt(5);
+				}
+				
+				System.out.println(enemy.getName() + " used " + enemy.getMove(rand2));
+				if (character.getCurHealth() <= 0){
+					System.out.println("It did " + battle.getMoveDamage(enemy, character) + " points of damage.");
+					System.out.println(enemy.getName() + " has won!");
+					break;
+				} else {
+					System.out.println("It did " + battle.getMoveDamage(enemy, character) + " points of damage.");
+					System.out.println(character.getName() + " has " + character.getCurHealth() + " points left");
+				}
+			} else if (character.getSpeed() < enemy.getSpeed()){
+				
+				battle.turnDamage(enemy, character);
+				int rand2 = random.nextInt(5);
+				while (rand2 == 0){
+					rand2 = random.nextInt(5);
+				}
+				
+				System.out.println(enemy.getName() + " used " + enemy.getMove(rand2));
+				if (character.getCurHealth() <= 0){
+					System.out.println("It did " + battle.getMoveDamage(enemy, character) + " points of damage.");
+					System.out.println(enemy.getName() + " has won!");
+					break;
+				} else {
+					System.out.println("It did " + battle.getMoveDamage(enemy, character) + " points of damage.");
+					System.out.println(character.getName() + " has " + character.getCurHealth() + " points left");
+				}
+				
+				battle.turnDamage(character, enemy);
+				System.out.println(character.getName() + " used " + character.getMove(move));
+				if (enemy.getCurHealth() <= 0){
+					System.out.println("It did " + battle.getMoveDamage(character, enemy) + " points of damage.");
+					System.out.println(character.getName() + " has won!");
+					break;
+				} else {
+					System.out.println("It did " + battle.getMoveDamage(character, enemy) + " points of damage.");
+					System.out.println(enemy.getName() + " has " + enemy.getCurHealth() + " points left");
+				}
+			}
+		}
+	}
+}
---
+++ src/Stats.java
@@ -0,0 +1,116 @@
+import java.util.Random;
+
+public class Stats {
+
+
+	/*Attributes*/
+	
+	public int curHealth = 100;
+	private int attack, defense, speed, health = 100;
+	private static int turn;
+	private String name, move1, move2, move3, move4;
+	
+	/*Constructor*/
+	
+	public Stats(int x){
+		if (x == 1){
+			name = "Mario";
+			attack = 20;
+			defense = 20;
+			speed = 20;
+			move1 = "Fireball";
+			move2 = "Cape";
+			move3 = "F.L.U.D.D.";
+			move4 = "Goomba Stomp";
+		}
+		else if (x == 2){
+			name = "Link";
+			attack = 18;
+			defense  = 22;
+			speed = 15;
+			move1 = "Pottery Abuse";
+			move2 = "Rupee Toss";
+			move3 = "Army of Chickens";
+			move4 = "Triangle Bitch";
+		}
+		else if (x == 3){
+			name = "Deadpool";
+			attack = 20;
+			defense = 18;
+			speed = 30;
+			move1 = "Excessive Enchilada";
+			move2 = "4th wall smack-down";
+			move3 = "Zamboni!!!";
+			move4 = "Sweet, Sweet Slicing";
+		}
+		else if (x == 4){
+			name = "Thomas Edison";
+			attack = 24;
+			defense = 16;
+			speed = 10;
+			move1 = "Killer Kite";
+			move2 = "Shocking Conclusion";
+			move3 = "Founding Father Fussing";
+			move4 = "Incandescent Bolt";
+		}
+	}
+	
+	/*Actions*/
+	public static void incTurn(){
+		turn++;
+	}
+	public int getAttack(){
+		return attack;
+	}
+	public int getDefense(){
+		return defense;
+	}
+	public int getSpeed(){
+		return speed;
+	}
+	public int getCurHealth(){
+		return curHealth;
+	}
+	public void setCurHealth(int x){
+		this.curHealth = x;
+	}
+	
+	public void decDefense(Stats character, Stats enemy){
+		Random random = new Random();
+		if(turn != 0){
+		character.defense -= random.nextInt(5);
+		enemy.defense -= random.nextInt(5);
+		}
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public String getMoves(){
+		String s = "";
+		s += "1." + this.getMove(1) + " 2." + this.getMove(2) + " 3." + this.getMove(3) + " 4." + this.getMove(4);
+		return s;
+	}
+	public String displayInfo(){
+		String s = "";
+		//s += "Name: " + name + "\n";
+		s += "Attack: " + attack + "\n" + "Defense: " + defense + "\n";
+		s += "Speed: " + speed + "\n" + "Health: " + health + "\n";
+		return s;
+	}
+	public String getMove(int x){
+		String move = "";
+		if (x == 1){
+			move = move1;
+		} else if (x == 2){
+			move = move2;
+		} else if (x == 3){
+			move = move3;
+		} else if (x == 4){
+			move = move4;
+		}
+		return move;
+	}
+
+
+}
